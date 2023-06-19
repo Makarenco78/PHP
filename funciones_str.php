76fb3701bd5bbc9b662c7a646ba29249c2e10187
Marcelo <marcelot78@hotmail.com>
2023-06-19 19:08:25 -0300

    agregando mas

--- funciones_str.php
+++ funciones_str.php
@@ -1,22 +1,33 @@
 ARRAY
 <?php
+
+"FUNCIONES ARRAY"
 $arreglo = array('H','O','L','A')
 $arreglo = [H,O,L,A]
-$arreglo=array_push($arreglo,'!')				/*agrega un elemento al final del arreglo*/
+$arreglo=array_push($arreglo,'!')			/*agrega un elemento al final*/
+$array_unshift($arreglo, $elm1, $elm2, ...)		/*agrega los elementos al comienz*/
+
 $arreglo=array_merge($arreglo1, $arreglo2)		/*combina arreglos*/
 $arreglo=array_reverse()
-$arreglo=array_map()							//-->averiguar que hace
+$arreglo=array_map()					//-->averiguar que hace
 $sub_arreglo=array_chunk($arreglo, 10)			/*divide un arreglo en n elementos*/
 $nombre=array_column($arreglo, $columna)
-
+$total=array_sum($arreglo)
 
 empty()
 
-implode()										//-->averiguar para que sirve
-array_unshift()									//-->averiguar que hace esto
+
+"OPERADOR TERNARIO"
+$bool=$array ? array_sum($array) : 0			/*Si el array tiene elementos revuelve la suma*/
+$bool=$array ? array_product($array) : 0		/*Si el array tiene elementos revuelve la multiplicacion*/
+
 
 
 "FUNCIONES STR"
+trim($string, " ")
+rtrim($string, " ")
+ltrim($string, " ")
+
 rand(0,1)
 random_int(0,10)
 ord()
@@ -27,8 +38,12 @@ strtolower
 shuffle()
 substr($texto, 0 , 10);
 range('a','z')
+	
+$arreglo=explode($delimitador, $string, $cantidad)	/*divide un string segun un caracter delimitador*/
+$string=implode(" ", $arreglo) 				/*une o concatena strings*/
+$string=join(' ', $arreglo)				/*similar a implode*/
 
-in_array(,)										//-->averiguar que hace esto
+in_array(,)						//-->averiguar que hace esto
 
 
 FUNCIONES GENERICAS

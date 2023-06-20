c8a1e5656935a129cb33e61829bd6fb44f500c3d
Marcelo <marcelot78@hotmail.com>
2023-06-19 21:39:02 -0300

    arreglando los archivos

--- arreglos.php
+++ arreglos.php
@@ -24,23 +24,6 @@ empty()
 $bool=$array ? array_sum($array) : 0			/*Si el array tiene elementos revuelve la suma*/
 $bool=$array ? array_product($array) : 0		/*Si el array tiene elementos revuelve la multiplicacion*/
 
-
-
-"FUNCIONES STR"
-trim($string, " ")
-rtrim($string, " ")
-ltrim($string, " ")
-
-rand(0,1)
-random_int(0,10)
-strrev()
-strlen()
-strtoupper()
-strtolower
-shuffle()
-substr($texto, 0 , 10);
-range('a','z')
-	
 $arreglo=explode($delimitador, $string, $cantidad)	/*divide un string segun un caracter delimitador*/
 $string=implode(" ", $arreglo) 				/*une o concatena strings*/
 $string=join(' ', $arreglo)				/*similar a implode*/
--- strings.php
+++ strings.php
@@ -1,29 +1,8 @@
-ARRAY
 <?php
 
-"FUNCIONES ARRAY"
-$arreglo = array('H','O','L','A')
-$arreglo = [H,O,L,A]
-$arreglo=array_push($arreglo,'!','!')			/*agrega los elementos al final*/
-$arreglo=array_pop($arreglo)				/*quita el ultimo elemento del arreglo*/
-
-$array_shift($arreglo)					/*quita un elemento al comienzo*/
-$array_unshift($arreglo, $elm1, $elm2, ...)		/*agrega los elementos al comienzo*/
-
-$arreglo=array_merge($arreglo1, $arreglo2)		/*combina arreglos*/
-$arreglo=array_reverse()
-$arreglo=array_map()					//-->averiguar que hace
-$sub_arreglo=array_chunk($arreglo, 10)			/*divide un arreglo en n elementos*/
-$nombre=array_column($arreglo, $columna)
-$total=array_sum($arreglo)
-
-empty()
-
-
 "OPERADOR TERNARIO"
-$bool=$array ? array_sum($array) : 0			/*Si el array tiene elementos revuelve la suma*/
-$bool=$array ? array_product($array) : 0		/*Si el array tiene elementos revuelve la multiplicacion*/
-
+$suma$array ? array_sum($array) : 0			/*Si el array tiene elementos revuelve la suma*/
+$suma=$array ? array_product($array) : 0		/*Si el array tiene elementos revuelve la multiplicacion*/
 
 
 "FUNCIONES STR"
@@ -44,8 +23,6 @@ range('a','z')
 $arreglo=explode($delimitador, $string, $cantidad)	/*divide un string segun un caracter delimitador*/
 $string=implode(" ", $arreglo) 				/*une o concatena strings*/
 $string=join(' ', $arreglo)				/*similar a implode*/
-
-in_array(,)						//-->averiguar que hace esto
 
 
 FUNCIONES GENERICAS

b530cb3b3bb21f196d81bb9565164bc426a1bd94
Marcelo <52867405+Makarenco78@users.noreply.github.com>
2023-06-21 00:19:04 -0300

    Add files via upload

---
+++ tarea10.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Iniciando configuracion de firewall - Ejercicio 10"
+
+#Rechazando las conexiones por la interfaz eth0
+sudo iptables -I INPUT -i eth0  -j DROP
+sudo iptables -A OUTPUT -o eth0  -j DROP
+
+#Permitiendo las conexiones por la interfaz eth1
+sudo iptables -A INPUT -i eth1  -j ACCEPT
+sudo iptables -A OUTPUT -o eth1  -j ACCEPT
+
+#sudo iptables -L -nv -line--numbers
+echo "Fin de la configuracion de firewall - Ejercicio 10"
